982f28af87d078e0da081da840ad0351572cbc2a
Jason <jason@dashmedia.com.au>
2017-05-24 15:22:13 +0930

    add support for files
    Signed-off-by: Jason <jason@dashmedia.com.au>

--- lib/CampaignMonitor_Send.php
+++ lib/CampaignMonitor_Send.php
@@ -3,7 +3,7 @@
 
 class CampaignMonitor_Send implements Email_Send
 {
-
+	private $files = array();
 	public function __construct($modx)
 	{
 		$this->modx = $modx;
@@ -17,6 +17,39 @@ class CampaignMonitor_Send implements Email_Send
 	public function setFields($fields){
 		$this->fields = $fields;
 	}
+	private function setFiles(){
+		foreach ($_FILES as $fieldName => $file) {
+	    $tmp_name = $_FILES[$fieldName]['tmp_name'];
+	    $type = $_FILES[$fieldName]['type'];
+	    $file_name = $_FILES[$fieldName]['name'];
+	    $size = $_FILES[$fieldName]['size'];
+
+	    if (file_exists($tmp_name)){
+
+	      // Check to make sure that it is an uploaded file and not a system file
+	      if(is_uploaded_file($tmp_name)){
+
+	         // Now Open the file for a binary read
+	         $file = fopen($tmp_name,'rb');
+
+	         // Now read the file content into a variable
+	         $data = fread($file,filesize($tmp_name));
+
+	         // close the file
+	         fclose($file);
+
+	         // Now we need to encode it and split it into acceptable length lines
+	         $data = base64_encode($data);
+
+					 $this->files[] = array(
+						 'Content'=> $data,
+						 'Name'=>$file_name,
+						 'Type'=>$type
+					 );
+	      }
+	    }
+	  }
+	}
 	public function setHtmlContent($content){
 		$this->rendered_html = $content;
 	}
@@ -24,7 +57,7 @@ class CampaignMonitor_Send implements Email_Send
 		$this->rendered_text_only = $content;
 	}
 	public function send($to, $from)
-	{	
+	{
 
 		$auth = $this->modx->getOption('formhandler.cm_api_key', null, null);
 		$templateId = $this->templateId;
@@ -41,11 +74,18 @@ class CampaignMonitor_Send implements Email_Send
 		if(!empty($this->subject)){
 			$data['email_subject'] = $this->subject;
 		}
-		
+
 		$message = array(
 			'To' => $to,
 			'Data' => $data
 			);
+
+		$this->setFiles();
+
+		if(!empty($this->files)){
+			$message['Attachments'] = $this->files;
+		}
+
 		$result = $cm_transactional_send->send($message);
 		if(!is_array($result->response)){
 			throw new Exception('CampaignMonitor_Send Error: '.$result->response->Message);
